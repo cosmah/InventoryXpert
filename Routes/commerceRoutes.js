bb6d8755c52cef313302d008ca32f8fd13e86789
cosmah <ssekirya.cosma@student.utamu.ac.ug>
2023-05-26 15:15:19 +0300

    working on the recept, successfuly inaftslled the api

--- Routes/commerceRoutes.js
+++ Routes/commerceRoutes.js
@@ -6,14 +6,15 @@ const Customer = require("../Models/customerModel");
 
 // add receipt page
 router.get('/commerce/addreciept', async (req, res) => {
-    try {
-      const customers = await Customer.find(); // Retrieve customers from the database
-      res.render('commerce/addreciept', { customers });
-    } catch (error) {
-      console.log(error);
-      res.status(500).send('Failed to fetch data from the database.');
-    }
-  });
+  try {
+    const customers = await Customer.find(); // Retrieve customers from the database
+    res.render('commerce/addreciept', { customers });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Failed to fetch data from the database.');
+  }
+});
+
 // Handle add receipt form submission
 router.post("/commerce/addreciept", async (req, res) => {
   try {
@@ -24,6 +25,18 @@ router.post("/commerce/addreciept", async (req, res) => {
   } catch (error) {
     res.status(400).send("Failed to add receipt. Please try again.");
     console.log(error);
+  }
+});
+
+// NEW: Endpoint for fetching customer name suggestions
+router.get('/customers/search', async (req, res) => {
+  try {
+    const searchQuery = req.query.q;
+    const customers = await Customer.find({ fname: new RegExp(searchQuery, 'i') });
+    res.json(customers);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Failed to fetch data from the database.');
   }
 });
 
