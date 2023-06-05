c214cf3dc4977f0ff8f860bde64897299832a88b
cosmah <ssekirya.cosma@student.utamu.ac.ug>
2023-06-05 14:08:45 +0300

    more routes

--- Routes/reportRoutes.js
+++ Routes/reportRoutes.js
@@ -23,5 +23,30 @@ const router = express.Router();
       res.render('reports/customers_sales');
   })
 
+//customers list
+  router.get("/reports/customers", (req,res)=>{
+      res.render('reports/customers');
+  })
+
+  //payments recieved
+  router.get("/reports/payments", (req,res)=>{
+      res.render('reports/payments');
+  })
+
+  //employees list
+  router.get("/reports/employeeList", (req,res)=>{
+      res.render('reports/employeeList');
+  })
+
+  //profits and losses 
+  router.get("/reports/profit_and_loss", (req,res)=>{
+      res.render('reports/profit_and_loss');
+  })
+
+  //sales details
+  router.get("/reports/salesDetails", (req,res)=>{
+      res.render('reports/salesDetails');
+  })
+
 
   module.exports = router
