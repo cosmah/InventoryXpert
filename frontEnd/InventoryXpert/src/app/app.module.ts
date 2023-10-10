211a1794dbbed088b5fc413f961f812859715c3e
cosmah <ssekirya.cosma@student.utamu.ac.ug>
2023-10-10 13:48:15 +0300

    working on the front

--- frontEnd/InventoryXpert/src/app/app.module.ts
+++ frontEnd/InventoryXpert/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavigatorComponent } from './navigator/navigator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavigatorComponent
   ],
   imports: [
     BrowserModule,
