d2b25e8da629314658d6eb9822fe66b3fb4df161
cosmah <ssekirya.cosma@student.utamu.ac.ug>
2023-10-18 15:07:54 +0300

    working on people compontns

--- frontEnd/InventoryXpert/src/app/app-routing.module.ts
+++ frontEnd/InventoryXpert/src/app/app-routing.module.ts
@@ -4,13 +4,52 @@ import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AddCustomerComponent } from './people/cus/add-customer/add-customer.component';
 import { PeopleComponent } from './people/people/people.component';
+import { CustomerComponent } from './people/cus/customer/customer.component';
+import { UpdateCustomerComponent } from './people/cus/update-customer/update-customer.component';
+import { AddSrComponent } from './people/sr/add-sr/add-sr.component';
+import { SalesmanComponent } from './people/sr/salesman/salesman.component';
+import { UpdateSrComponent } from './people/sr/update-sr/update-sr.component';
+import { AddSupplierComponent } from './people/sup/add-supplier/add-supplier.component';
+import { SuppliersComponent } from './people/sup/suppliers/suppliers.component';
+import { UpdateSupplierComponent } from './people/sup/update-supplier/update-supplier.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'add-customer', component: AddCustomerComponent},
-  { path: 'people', component: PeopleComponent},
+  { path: 'people', component: PeopleComponent, children: [
+    // Customer components
+    { path: 'customer', component: CustomerComponent },
+    { path: 'add-customer', component: AddCustomerComponent },
+    { path: 'update-customer', component: UpdateCustomerComponent },
+
+    // Salesman components
+    { path: 'salesman', component: SalesmanComponent },
+    { path: 'add-sr', component: AddSrComponent },
+    { path: 'update-sr', component: UpdateSrComponent },
+
+    // Supplier components
+    { path: 'suppliers', component: SuppliersComponent },
+    { path: 'add-supplier', component: AddSupplierComponent },
+    { path: 'update-supplier', component: UpdateSupplierComponent },
+  ]},
+
+  //customer components
+  { path: 'add-customer', component: AddCustomerComponent},
+  { path: 'customer', component: CustomerComponent},
+  { path: 'update-customer', component: UpdateCustomerComponent},
+
+  //salesmen components
+  { path: 'add-sr', component: AddSrComponent},
+  { path: 'salesman', component: SalesmanComponent},
+  { path: 'update-sr', component: UpdateSrComponent},
+
+  //supplier components
+  { path: 'add-supplier', component: AddSupplierComponent},
+  { path: 'suppliers', component: SuppliersComponent},
+  { path: 'update-supplier', component: UpdateSupplierComponent},
+
 ];
 
 @NgModule({
--- frontEnd/InventoryXpert/src/app/app.module.ts
+++ frontEnd/InventoryXpert/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FoldableModule } from 'ngx-foldable';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -42,7 +45,10 @@ import { PeopleComponent } from './people/people/people.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FoldableModule
+    FoldableModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
