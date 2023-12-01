943cd93cd03af8cc72b48871344c4866dd4a44a6
cosmah <ssekirya.cosma@student.utamu.ac.ug>
2023-12-01 15:21:20 +0300

    added exporting of the products list via excel

--- inventoyxpert/src/main/java/com/inventoryxpert/application/views/products/Inventory.java
+++ inventoyxpert/src/main/java/com/inventoryxpert/application/views/products/Inventory.java
@@ -3,20 +3,29 @@ package com.inventoryxpert.application.views.products;
 import com.inventoryxpert.application.backend.entity.Product;
 import com.inventoryxpert.application.backend.service.ProductService;
 import com.inventoryxpert.application.views.MainLayout;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.data.binder.Binder.BindingBuilder;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.vaadin.flow.server.StreamResource;
+import com.vaadin.flow.component.html.Anchor;
 
+import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.List;
 
 @PageTitle("Products")
@@ -27,7 +36,7 @@ public class Inventory extends VerticalLayout {
     private Grid<Product> grid = new Grid<>(Product.class);
     private final ProductService productService;
     private TextField filterText = new TextField();
-    //private Binder<Product> binder = new Binder<>(Product.class);
+    // private Binder<Product> binder = new Binder<>(Product.class);
     private ActionsForm form;
 
     @Autowired
@@ -51,11 +60,21 @@ public class Inventory extends VerticalLayout {
         // Fetch data from ProductService and set it in the grid
         populateGrid();
 
+        // Create the download button
+        Anchor downloadButton = createDownloadButton();
+
+        // Create a layout for the filter text field and the download button
+        HorizontalLayout topLayout = new HorizontalLayout(filterText, downloadButton);
+        topLayout.setWidthFull();
+        topLayout.setJustifyContentMode(JustifyContentMode.END);
+
         // Add the form and grid to the layout
         Div content = new Div(grid, form);
         content.addClassName("content");
         content.setSizeFull();
-        add(filterText, content);
+
+        // Add the top layout and the content to the layout
+        add(topLayout, content);
 
         updateList();
         closeEditor();
@@ -68,13 +87,12 @@ public class Inventory extends VerticalLayout {
 
     }
 
-    //saveContact method
+    // saveContact method
     private void saveProduct(ActionsForm.SaveEvent event) {
         productService.save(event.getProduct());
         updateList();
         closeEditor();
     }
-
 
     private void updateList() {
         List<Product> products = productService.findAll(filterText.getValue());
@@ -95,8 +113,7 @@ public class Inventory extends VerticalLayout {
         grid.setColumns("productCode", "productName", "quantity",
                 "price", "resalePrice", "supplier");
 
-        grid.asSingleSelect().addValueChangeListener(event ->
-                editProduct(event.getValue()));
+        grid.asSingleSelect().addValueChangeListener(event -> editProduct(event.getValue()));
     }
 
     private void editProduct(Product product) {
@@ -125,7 +142,59 @@ public class Inventory extends VerticalLayout {
         // Set the fetched data in the grid
         grid.setItems(productList);
 
-        //configure size
+        // configure size
         grid.getColumns().forEach(col -> col.setAutoWidth(true));
     }
+
+    // Step 2: Create method to generate Excel file
+    public ByteArrayInputStream productsToExcel(List<Product> products) throws IOException {
+        String[] columns = { "ID", "Product Name", "Product Code", "Product Description", "Price", "Quantity",
+                "Starting Date", "Supplier", "Resale Price" };
+        try (
+                Workbook workbook = new XSSFWorkbook();
+                ByteArrayOutputStream out = new ByteArrayOutputStream();) {
+            CreationHelper createHelper = workbook.getCreationHelper();
+            Sheet sheet = workbook.createSheet("Products");
+            Font headerFont = workbook.createFont();
+            headerFont.setBold(true);
+            CellStyle headerCellStyle = workbook.createCellStyle();
+            headerCellStyle.setFont(headerFont);
+            Row headerRow = sheet.createRow(0);
+            for (int col = 0; col < columns.length; col++) {
+                Cell cell = headerRow.createCell(col);
+                cell.setCellValue(columns[col]);
+                cell.setCellStyle(headerCellStyle);
+            }
+            int rowIdx = 1;
+            for (Product product : products) {
+                Row row = sheet.createRow(rowIdx++);
+                row.createCell(0).setCellValue(product.getId());
+                row.createCell(1).setCellValue(product.getProductName());
+                row.createCell(2).setCellValue(product.getProductCode());
+                row.createCell(3).setCellValue(product.getProductDescription());
+                row.createCell(4).setCellValue(product.getPrice());
+                row.createCell(5).setCellValue(product.getQuantity());
+                row.createCell(6).setCellValue(product.getStartingDate().toString());
+                row.createCell(7).setCellValue(product.getSupplier());
+                row.createCell(8).setCellValue(product.getResalePrice());
+            }
+            workbook.write(out);
+            return new ByteArrayInputStream(out.toByteArray());
+        }
+    }
+
+    private Anchor createDownloadButton() {
+        StreamResource resource = new StreamResource("products.xlsx", () -> {
+            try {
+                return productsToExcel(productService.findAll());
+            } catch (IOException e) {
+                e.printStackTrace();
+                return new ByteArrayInputStream(new byte[0]);
+            }
+        });
+        Anchor downloadButton = new Anchor(resource, "Download");
+        downloadButton.getElement().setAttribute("download", true);
+        return downloadButton;
+    }
+
 }
